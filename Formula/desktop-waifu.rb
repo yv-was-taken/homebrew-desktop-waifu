626dc05fac77a79383db6c16e698573a4558d0ae
yv-was-taken <yvmail@proton.me>
2026-01-25 16:01:25 -0600

    Update to v0.2.3

--- Formula/desktop-waifu.rb
+++ Formula/desktop-waifu.rb
@@ -4,8 +4,8 @@
 class DesktopWaifu < Formula
   desc "Animated 3D VRM characters with AI-powered conversational chat"
   homepage "https://github.com/yv-was-taken/desktop-waifu"
-  url "https://github.com/yv-was-taken/desktop-waifu/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "9960a619eef805d6051f2a9b30c7f7078e8a21258f9f990abc5a217a98ae9876"
+  url "https://github.com/yv-was-taken/desktop-waifu/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "a30994ec8a2501a42998ee2280e18c83973105406c13d442f47d1cfd6e174596"
   license "MIT"
   head "https://github.com/yv-was-taken/desktop-waifu.git", branch: "master"
 
